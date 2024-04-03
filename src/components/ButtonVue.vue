93f82fa60b5ab2d953363880b39ff1f5c0b166d7
farahaudina <farah06102006@gmail.com>
2024-04-03 21:05:12 +0700

    first

---
+++ src/components/ButtonVue.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- Tombol untuk memicu pop-up -->
+    <button @click="showPopup = true">Klik</button>
+
+    <!-- Pop-up -->
+    <div v-if="showPopup" class="popup">
+      <div class="popup-content">
+        <!-- Konten Pop-up -->
+        <h2>My Instagram</h2>
+        <a href="https://www.instagram.com/f_audina/">Coba Klik</a>
+        <!-- Tombol untuk menutup pop-up -->
+        <button @click="showPopup = false">Tutup Pop-up</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPopup: false // Status untuk menampilkan atau menyembunyikan pop-up
+    };
+  }
+};
+</script>
+
+<style>
+/* Gaya untuk pop-up */
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Transparan hitam sebagai latar belakang */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,42 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Biodata Diri</template>
+
+   Halo! Perkenalkan saya Farah, saya berasal dari Kota Dumai, Riau. Saya merupakan mahasiswa di Universitas Islam Riau.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Skill & Kemampuan</template>
+
+    - Memasak <br>
+    - Menari <br>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Pengalaman Yang Pernah Dimiliki</template>
+
+    - Saya pernah mengikuti lomba menari saat TK <br>
+    - Saya pernah mengikuti OSN Kimia saat SMA <br>
+  </WelcomeItem>
+
+  
+ 
+</template>
